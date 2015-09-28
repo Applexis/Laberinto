a8a59a6c65f671476356c7661930ee6360a6a3e8
Applex <alex.nantong@gmail.com>
2015-09-28 15:21:09 +0800

    Add observer design patter example

---
+++ Observer/ConcreateObserver.java
@@ -0,0 +1,10 @@
+public class ConcreteObserver implements Observer {
+    private String observerState;
+    
+    @Override
+    public void update(String state) {
+        observerState = state;
+        System.out.println("状态为：" + observerState);
+    }
+
+}
---
+++ Observer/Subject.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Subject {
+
+    private List<Observer> list = new ArrayList<Observer>();
+
+    public void attach(Observer observer){
+        list.add(observer);
+        System.out.println("Attached an observer");
+    }
+
+    public void detach(Observer observer){
+        list.remove(observer);
+    }
+
+    public void nodifyObservers(String newState){
+        
+        for(Observer observer : list){
+            observer.update(newState);
+        }
+    }
+}
